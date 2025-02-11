15d279ccf4ed5c001488416a79b333bd97c98674
Christian <cs0097746@gmail.com>
2025-02-11 12:01:41 -0300

    criando form para crud cliente

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+
 from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
-]
+    path('search/', views.search, name='search'),
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+]   
